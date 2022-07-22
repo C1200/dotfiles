ae4b058614057edc8e3b21273ffe8f57bf71147c
Corey <40738690+C1200@users.noreply.github.com>
2022-07-22 20:09:00 +0100

    make .gitignore work

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@
 const fs = require("fs");
 const path = require("path");
 
+const noDot = ["gitignore"]
 const files = path.join(__dirname, "..", "files");
 
 fs.readdirSync(files).forEach(file => {
     const content = fs.readFileSync(path.join(files, file));
-    fs.writeFileSync(path.join(process.cwd(), file), content);
+    fs.writeFileSync(path.join(process.cwd(), (noDot.includes(file) ? "." : "") + file), content);
 });

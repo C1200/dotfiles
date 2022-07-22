b2bd3fa9e2994f7844c3f752b9e8fddc7d423f28
Corey <40738690+C1200@users.noreply.github.com>
2022-07-22 19:43:45 +0100

    Init Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+
+const files = path.join(__dirname, "./files");
+
+fs.readdirSync(files).forEach(file => {
+    const content = fs.readFileSync(path.join(files, file));
+    fs.writeFileSync(path.join(process.cwd(), file), content);
+});

d719e2f3fe809a6c9005b91c2671df78c0aca3e2
C1200 <ogjtudjy@gmail.com>
2023-02-26 09:57:34 +0000

    Fix bug

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ const noDot = ["gitignore"]
 const files = path.join(__dirname, "..", "files");
 
 fs.readdirSync(files).forEach(file => {
-    const content = fs.readFileSync(path.join(files, file)).replace(/\$YEAR/g, new Date().getFullYear());
+    const content = fs.readFileSync(path.join(files, file), 'utf8').replace(/\$YEAR/g, new Date().getFullYear());
     fs.writeFileSync(path.join(process.cwd(), (noDot.includes(file) ? "." : "") + file), content);
 });
